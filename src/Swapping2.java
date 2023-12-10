1e6f0483a1e506b574a17b68b78c17d942aa4345
Rahul kumar maury <rahu7827759@gmail.com>
2023-12-10 20:36:57 +0530

    1. How to find Factorial in java. 2. How to Swap two number without using 3rd variable. 3. How to swap two number without using + and - operator.

---
+++ src/Swapping2.java
@@ -0,0 +1,20 @@
+
+// How to swap two number without using + and - operator
+public class Swapping2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        //Before swapping
+        System.out.println("Before swapping: -> " + a + " " + b);
+
+        // Swapping logic using XOR (^) operator
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        //After swapping
+        System.out.println("After swapping:  -> " + a + " " + b);
+
+    }
+}
