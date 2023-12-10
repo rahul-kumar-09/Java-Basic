9be921c3c5d283db068976b5a42e3941e1843d6b
Rahul kumar maury <rahu7827759@gmail.com>
2023-12-10 16:42:50 +0530

    How to find Factorial in java

---
+++ src/factorial.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class factorial {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number for Factorial: ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+        for (int i=num; i>=1; i--){
+            fact = fact * i;
+        }
+        System.out.println("Factorail is " + fact);
+
+    }
+}
